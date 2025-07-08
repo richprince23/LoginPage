f5f6d8dd5a0d92bb1d970341363b6f9856ce6937
Richard Kweku Aikins <richardkns7@gmail.com>
2025-07-08 20:52:47 +0000

    text

--- LoginPage/ContentView.swift
+++ LoginPage/ContentView.swift
@@ -8,16 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var userName:String = "";
+    @State var password = "";
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            //            Text("\($userName.wrappedValue)")
+            Text("Hello World, my name is Aikins, and i am learning Swift UI").font(.system(size: 16, design: .serif)).multilineTextAlignment(.center)
+                .baselineOffset(20)
+                .kerning(1).foregroundStyle(.red)
+                .frame(width: 200, height: 100)
+                .minimumScaleFactor(0.1)
         }
         .padding()
     }
+ 
+    func printDetails(){
+        userName = userName + " " + password
+    }
 }
+
+
 
 #Preview {
     ContentView()
