fe3f11e0c02d8206b0a6e8d0b0a73cedeadf0abc
Richard Kweku Aikins <richardkns7@gmail.com>
2025-07-09 00:22:50 +0000

    Gradients

---
+++ LoginPage/Gradients.swift
@@ -0,0 +1,18 @@
+//
+//  Gradients.swift
+//  LoginPage
+//
+//  Created by Richard Kweku Aikins on 09/07/2025.
+//
+
+import SwiftUI
+
+struct Gradients: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Gradients()
+}
