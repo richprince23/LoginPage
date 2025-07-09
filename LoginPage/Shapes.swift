b060411157faf8dbe0a31cd43cdd277d188b8c99
Richard Kweku Aikins <richardkns7@gmail.com>
2025-07-09 00:01:59 +0000

    colors

---
+++ LoginPage/Shapes.swift
@@ -0,0 +1,18 @@
+//
+//  Shapes.swift
+//  LoginPage
+//
+//  Created by Richard Kweku Aikins on 08/07/2025.
+//
+
+import SwiftUI
+
+struct Shapes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Shapes()
+}
